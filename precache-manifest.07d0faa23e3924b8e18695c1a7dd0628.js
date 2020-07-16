7c41fbb54ab38d70a70cc48a1b4c592750ea9d16
Babken Nurijanyan <babkenn@gmail.com>
2020-07-16 10:32:54 -0400

    Updates

---
+++ precache-manifest.07d0faa23e3924b8e18695c1a7dd0628.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4e7c16e5ccccd2d76703bfad0557cf",
+    "url": "/CoverPage%20/index.html"
+  },
+  {
+    "revision": "a75dc9f6483a56a0d13a",
+    "url": "/CoverPage%20/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a75dc9f6483a56a0d13a",
+    "url": "/CoverPage%20/static/js/2.a02c65f1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CoverPage%20/static/js/2.a02c65f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1679236824d0ffd800",
+    "url": "/CoverPage%20/static/js/main.4cfdf06a.chunk.js"
+  },
+  {
+    "revision": "c21bbe7ed515141c2ee2",
+    "url": "/CoverPage%20/static/js/runtime-main.f42d7c8b.js"
+  },
+  {
+    "revision": "f2738878d4fac98e7c4748310c995720",
+    "url": "/CoverPage%20/static/media/Capture.f2738878.PNG"
+  },
+  {
+    "revision": "a08176df54a98bcd608ad7bb8f7372db",
+    "url": "/CoverPage%20/static/media/GitHub.a08176df.png"
+  },
+  {
+    "revision": "a6de4bee1a9283fe872cbb9be2e11cae",
+    "url": "/CoverPage%20/static/media/OpenCV.a6de4bee.png"
+  },
+  {
+    "revision": "5282daa164b1dfcf3ba649f2ab216206",
+    "url": "/CoverPage%20/static/media/lnk.5282daa1.png"
+  }
+]);
